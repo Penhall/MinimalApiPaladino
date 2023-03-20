e5f3dec8366704937ce42b6094bbc83e41e08c5d
Penhall <penhall@gmail.com>
2023-03-20 20:18:52 -0300

    aalterando string de conexão

--- MinimalApiPaladino/Program.cs
+++ MinimalApiPaladino/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApiPaladino.Data;
 using MinimalApiPaladino.Endpoints;
 
 
@@ -6,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddSwagger();
 builder.Services.AddCors();
 builder.AddPersistence();
+
+var connectionString = builder.Configuration.GetConnectionString("\"SqliteConnectionString");
+builder.Services.AddDbContext<PaladinoDbContext>(option => option.UseSqlite(connectionString));
 
 var app = builder.Build();
 
